96b190cccc307d8319616a5ce8595d827786c9e8
nemusagi <rinyoshida25@gmail.com>
2022-07-27 15:06:34 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,5 @@ class User < ApplicationRecord
     validates :last_name_kana
   end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
