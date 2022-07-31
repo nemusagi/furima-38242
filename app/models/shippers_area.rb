cfb9e1fc58356d068b937ad3097a5c7f6d3267de
nemusagi <rinyoshida25@gmail.com>
2022-07-31 14:17:21 +0900

    endの位置の修正

--- app/models/shippers_area.rb
+++ app/models/shippers_area.rb
@@ -53,4 +53,4 @@ class ShippersArea < ActiveHash::Base
   include ActiveHash::Associations
   has_many :items
 
-  end
+end
--- app/models/shipping_charge.rb
+++ app/models/shipping_charge.rb
@@ -8,4 +8,4 @@ class ShippingCharge < ActiveHash::Base
   include ActiveHash::Associations
   has_many :items
 
-  end
+end
