b335e6bb769c1712004fdb6008847c42cdb6cc02
nemusagi <rinyoshida25@gmail.com>
2022-07-31 21:27:24 +0900

    商品情報編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create, :edit, :update ]
+  before_action :set_item, only: [ :show, :edit, :update ]
   before_action :move_to_index, only:  [ :edit, :update ]
-  before_action :set_item, only: [:edit, :update ]
   def index
     @items =Item.order("created_at DESC")
   end
@@ -18,11 +18,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -45,7 +43,6 @@ class ItemsController < ApplicationController
   end
   
   def move_to_index
-    @item = Item.find(params[:id])
     unless  current_user.id ==  @item.user_id
       redirect_to action: :index
     end
